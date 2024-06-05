3d8cce0e16bcd9eea7c48656134617260a57afe1
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-06-05 16:15:43 +0000

    for, break and continue

---
+++ 27_ForLoop/breakAndContinue.js
@@ -0,0 +1,22 @@
+// +++++ break and continue +++++
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 5) {
+//     console.log("Detected 5");
+//     break;
+//   }
+//   console.log(`Value of i is: ${i}`);
+//   Value of i is: 1
+//   Value of i is: 2
+//   Value of i is: 3
+//   Value of i is: 4
+//   Detected 5
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 5) {
+//     console.log("Detected 5");
+//     continue;
+//   }
+//   console.log(`Value of i is: ${i}`); // 5 will not print, other values will print from 0 to 20
+// }
